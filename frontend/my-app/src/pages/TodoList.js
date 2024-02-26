9003e232bc81fa84d932f350c28a75bd3e298099
GAEUN JEONG <79949390+rkdms357@users.noreply.github.com>
2024-02-26 20:44:28 +0900

    투두리스트 구현 완료

---
+++ frontend/my-app/src/pages/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../src/components/TodoListForm/TodoListForm.css';
+
+const TodoList = () => {
+    return (
+        <div className='todo-title'>
+            <h1>TodoList&nbsp;</h1>
+            <a href='/Account'>my account</a>
+        </div>
+    );
+};
+
+export default TodoList;
